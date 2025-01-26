6b4f9cb5327b0e316051d3c95b735cc865a7f596
alderjunio <alder.junio@gmail.com>
2025-01-26 14:43:45 -0400

    finalizado com dados mocados

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+   { "id": "1",    
+    "title": "Notícia",
+    "description": "descreição 01",
+    "photoCover": "https://cdn-cliquediario.nuneshost.com/wp-content/uploads/2025/01/WhatsApp-Image-2025-01-10-at-11.16.01-1-1280x720.jpeg"
+   },
+   { "id": "2",    
+    "title": "O que é Java Spring?",
+    "description": "Java Spring, ou Spring Framework, é um dos frameworks mais populares no desenvolvimento Java. Ele oferece ferramentas para criar aplicativos robustos, escaláveis e fáceis de manter. Com foco na injeção de dependência (DI) e inversão de controle (IoC), o Spring ajuda a criar aplicativos modulares e testáveis. Ele inclui módulos como Spring Boot, para simplificar configurações, Spring MVC, para construir aplicações web, Spring Security, para autenticação e autorização, e Spring Data, que facilita a integração com bancos de dados. Aprender Spring pode ser essencial para quem busca desenvolver soluções de alta performance e qualidade.",
+    "photoCover": "https://i.ytimg.com/vi/arqIpTiIKfI/maxresdefault.jpg"
+   },
+   { "id": "3",    
+    "title": "Você ainda usa esse Framework?",
+    "description": "descricao 03",
+    "photoCover": "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/sites/152896/images/OzA7RhDQHOlsIrnB5LqQ_file.jpg"
+   },
+]
